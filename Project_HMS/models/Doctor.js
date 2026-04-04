ac383d44d4e3ff5500985dee94a7026e223c488a
Sandip Maity <145787637+Sandip-Maity-2023@users.noreply.github.com>
2026-04-05 01:16:57 +0530

    Delete Project_HMS directory

--- Project_HMS/models/Doctor.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const DoctorSchema=new mongoose.Schema({
-    name: String,
-    specialization: String,
-    doctor_id:{type:String,unique:true},
-});
-
-module.exports=mongoose.model('Doctor',DoctorSchema);
